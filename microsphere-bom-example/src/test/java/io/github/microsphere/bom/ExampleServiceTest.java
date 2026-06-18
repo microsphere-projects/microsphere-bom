723256a2f370197450218384c50c18365ef75e96
Mercy Ma <mercyblitz@gmail.com>
2026-06-18 17:40:10 +0800

    Convert example to BOM and rename package
    Switch the example module to use the microsphere-bom parent and add dependencyManagement importing microsphere-all-bom. Update artifactId, name and description accordingly; remove junit.version property and various profiles (docs, java8-16) since versions and plugins are now managed by the BOM. Also rename the ExampleService and its test package from io.github.microsphere.example to io.github.microsphere.bom to reflect the module reorganization.

--- microsphere-bom-example/src/test/java/io/github/microsphere/bom/ExampleServiceTest.java
+++ microsphere-bom-example/src/test/java/io/github/microsphere/bom/ExampleServiceTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.github.microsphere.example;
+package io.github.microsphere.bom;
 
 import org.junit.jupiter.api.Test;
 
